863b96c39f4744191d3111c901d406bf33dd6cac
Clifford-La <keishi.la@education.nsw.gov.au>
2026-03-12 21:56:12 +0000

    green coloured output
    \

--- Periodic Table Classification.py
+++ Periodic Table Classification.py
@@ -1,17 +1,16 @@
 #Periodic Table element classification
 import json
-
+green = "\033[32m"
 def main():
     element_input = input("What element are you looking for? ").title()
     element, element_dict = element_loader(element_input)
     if element in element_dict:
-        print(f"Element: {element}")
+        print(f"{green}Element: {element}")
 
 def element_loader(element_input):
     with open("element.json", "r") as file:
         element_dict = json.load(file)
     for element in element_dict:
         if element['Name'] == element_input:
-            return element, element_dict
-        
+            return element, element_dict 
 main()
